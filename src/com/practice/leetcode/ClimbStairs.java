3206242b8ba5169c64ffe861a00a3fe8ea11af03
preetiparnasahoo <49512342+preetiparnasahoo@users.noreply.github.com>
2024-08-29 08:39:20 +0530

    Previous code checkin

---
+++ src/com/practice/leetcode/ClimbStairs.java
@@ -0,0 +1,10 @@
+package com.practice.leetcode;
+
+public class ClimbStairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/practice/leetcode/PowerOf2.java
@@ -0,0 +1,14 @@
+package com.practice.leetcode;
+
+public class PowerOf2 {
+
+	public static void main(String[] args) {
+
+		int input = 100;
+		
+		for (int i = 1; i <= input ; i = i * 2) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
